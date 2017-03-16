c993f016a33d59f99a76e66f7c4707d06ca70bdd
Frankie Jarrett <fjarrett@gmail.com>
2017-03-16 09:42:32 -0500

    Various workflow improvements (#8)
    * Update readme with grunt instructions
    
    * Improve tests to use dataProvider
    
    * Update pre-commit hook and how-to readme
    
    * Use composer for phpunit
    
    * Silence stupid npm warning
    
    * Not so verbose in travis
    
    * Convert all demos URLs to use HTTPS
    
    * Update hook
    
    * Show git changes on pre-commit

--- readme.md
+++ readme.md
@@ -6,6 +6,7 @@ Manifest of all WordPress Themes created by GoDaddy.
 
 1. Clone this repo
 2. Run `cp pre-commit .git/hooks/pre-commit`
-3. Make changes to `manifest.json` on `develop`
-4. Commit and push
-5. Issue a PR against `master` (requires peer approval to merge)
+3. Run `git checkout develop` and **ONLY** make changes to `manifest.json`
+4. Commit your changes (`grunt` and unit tests will run automatically)
+5. If the tests pass, do a `git push`
+6. Issue a PR against `master` (requires peer approval to merge)
--- tests.php
+++ tests.php
@@ -6,7 +6,16 @@ class Tests extends TestCase
 {
 	public static $manifest     = __DIR__ . '/manifest.json';
 	public static $manifest_min = __DIR__ . '/manifest.min.json';
-	public static $themes       = [];
+
+	public static function provideThemeData()
+	{
+		return array_map(
+			function ( $v ) {
+				return [ $v ];
+			},
+			json_decode( file_get_contents( self::$manifest ), true )
+		);
+	}
 
 	public function testFilesExist()
 	{
@@ -27,64 +36,66 @@ class Tests extends TestCase
 
 		$this->assertNotEmpty( $manifest, 'Manifest is empty' );
 		$this->assertNotEmpty( $manifest_min, 'Minified manifest is empty' );
-		$this->assertTrue( $manifest === $manifest_min, 'Manifests do not match' );
-
-		self::$themes = $manifest;
+		$this->assertEquals( $manifest, $manifest_min, 'Manifests do not match' );
 	}
 
-	public function testThemeData()
+	/**
+	 * @dataProvider provideThemeData
+	 */
+	public function testThemeData( $data )
 	{
-		array_walk( self::$themes, function ( $data ) {
-			$this->assertTrue( is_array( $data ), 'Theme data is not an array' );
-			$this->assertCount( 6, $data, 'Theme data array does not contain exactly 6 items' );
+		$this->assertTrue( is_array( $data ), 'Theme data is not an array' );
+		$this->assertCount( 6, $data, 'Theme data array does not contain exactly 6 items' );
 
-			$this->assertNotEmpty( $data['theme'], 'Theme slug does not exist' );
-			$this->assertTrue( ctype_lower( str_replace( '-', '', $data['theme'] ) ), 'Theme slug is not lowercase' );
+		$this->assertNotEmpty( $data['theme'], 'Theme slug does not exist' );
+		$this->assertTrue( ctype_lower( str_replace( '-', '', $data['theme'] ) ), 'Theme slug is not lowercase' );
 
-			$this->assertNotEmpty( $data['new_version'], 'Theme version does not exist' );
-			$this->assertRegExp( '/^([\w\.\-]+)$/', $data['new_version'], 'Theme version format is invalid' );
+		$this->assertNotEmpty( $data['new_version'], 'Theme version does not exist' );
+		$this->assertRegExp( '/^([\w\.\-]+)$/', $data['new_version'], 'Theme version format is invalid' );
 
-			$this->assertNotEmpty( $data['url'], 'Theme URL does not exist' );
-			$this->assertRegExp( '/^(https?):\/\/[^\s\/$.?#].[^\s]*$/i', $data['url'], 'Theme URL format is invalid' );
-			$this->assertContains( $data['theme'], $data['url'], 'Theme URL does not contain the theme slug' );
+		$this->assertNotEmpty( $data['url'], 'Theme URL does not exist' );
+		$this->assertRegExp( '/^(https?):\/\/[^\s\/$.?#].[^\s]*$/i', $data['url'], 'Theme URL format is invalid' );
+		$this->assertContains( $data['theme'], $data['url'], 'Theme URL does not contain the theme slug' );
 
-			$this->assertNotEmpty( $data['package'], 'Theme package URL does not exist' );
-			$this->assertRegExp( '/^(https?):\/\/[^\s\/$.?#].[^\s]*$/i', $data['package'], 'Theme package URL format is invalid' );
-			$this->assertContains( "/godaddy/wp-{$data['theme']}-theme/", $data['package'], 'Theme package URL does not contain the theme repo' );
-			$this->assertContains( "/v{$data['new_version']}/", $data['package'], 'Theme package URL does not contain the theme version' );
-			$this->assertStringEndsWith( "/{$data['theme']}.zip", $data['package'], 'Theme package URL does not point to a ZIP file of the theme slug' );
+		$this->assertNotEmpty( $data['package'], 'Theme package URL does not exist' );
+		$this->assertRegExp( '/^(https?):\/\/[^\s\/$.?#].[^\s]*$/i', $data['package'], 'Theme package URL format is invalid' );
+		$this->assertContains( "/godaddy/wp-{$data['theme']}-theme/", $data['package'], 'Theme package URL does not contain the theme repo' );
+		$this->assertContains( "/v{$data['new_version']}/", $data['package'], 'Theme package URL does not contain the theme version' );
+		$this->assertStringEndsWith( "/{$data['theme']}.zip", $data['package'], 'Theme package URL does not point to a ZIP file of the theme slug' );
 
-			$this->assertNotEmpty( $data['screenshot'], 'Theme screenshot URL does not exist' );
-			$this->assertRegExp( '/^(https?):\/\/[^\s\/$.?#].[^\s]*$/i', $data['screenshot'], 'Theme screenshot URL format is invalid' );
-			$this->assertContains( "/v{$data['new_version']}/", $data['screenshot'], 'Theme screenshot URL does not contain the theme version' );
-			$this->assertStringEndsWith( "/screenshot.png", $data['screenshot'], 'Theme screenshot URL does not point to a `screenshot.png` file' );
+		$this->assertNotEmpty( $data['screenshot'], 'Theme screenshot URL does not exist' );
+		$this->assertRegExp( '/^(https?):\/\/[^\s\/$.?#].[^\s]*$/i', $data['screenshot'], 'Theme screenshot URL format is invalid' );
+		$this->assertContains( "/v{$data['new_version']}/", $data['screenshot'], 'Theme screenshot URL does not contain the theme version' );
+		$this->assertStringEndsWith( "/screenshot.png", $data['screenshot'], 'Theme screenshot URL does not point to a `screenshot.png` file' );
 
-			$this->assertNotEmpty( $data['name'], 'Theme name does not exist' );
-			$this->assertEquals( strtolower( str_replace( ' ', '-', $data['name'] ) ), $data['theme'], 'Theme name and slug are not similar' );
-		} );
+		$this->assertNotEmpty( $data['name'], 'Theme name does not exist' );
+		$this->assertEquals( strtolower( str_replace( ' ', '-', $data['name'] ) ), $data['theme'], 'Theme name and slug are not similar' );
 	}
 
-	public function testUrlsReachable()
+	/**
+	 * @dataProvider provideThemeData
+	 */
+	public function testUrlReachable( $data )
 	{
-		array_walk( self::$themes, function ( $data ) {
-			$headers = get_headers( $data['url'] );
-			$this->assertContains( '200 OK', $headers[0], 'Theme URL is unreachable' );
-		} );
+		$headers = get_headers( $data['url'] );
+		$this->assertContains( '200 OK', $headers[0], 'Theme URL is unreachable' );
 	}
 
-	public function testPackgesReachable()
+	/**
+	 * @dataProvider provideThemeData
+	 */
+	public function testPackgeReachable( $data )
 	{
-		array_walk( self::$themes, function ( $data ) {
-			$headers = get_headers( $data['package'] );
-			$this->assertContains( '302 Found', $headers[0], 'Theme package URL is unreachable' );
-		} );
+		$headers = get_headers( $data['package'] );
+		$this->assertContains( '302 Found', $headers[0], 'Theme package URL is unreachable' );
 	}
 
-	public function testScreenshotsReachable()
+	/**
+	 * @dataProvider provideThemeData
+	 */
+	public function testScreenshotReachable( $data )
 	{
-		array_walk( self::$themes, function ( $data ) {
-			$headers = get_headers( $data['screenshot'] );
-			$this->assertContains( '200 OK', $headers[0], 'Theme screenshot URL is unreachable' );
-		} );
+		$headers = get_headers( $data['screenshot'] );
+		$this->assertContains( '200 OK', $headers[0], 'Theme screenshot URL is unreachable' );
 	}
 }
