05249e69907af46c0872d7164897c2f43e846dc1
Frankie Jarrett <fjarrett@gmail.com>
2019-10-02 18:02:58 -0500

    update phpunit, update tests, update theme data

--- tests.php
+++ tests.php
@@ -53,20 +53,17 @@ class Tests extends TestCase
 		$this->assertNotEmpty( $data['new_version'], 'Theme version does not exist' );
 		$this->assertRegExp( '/^([\w\.\-]+)$/', $data['new_version'], 'Theme version format is invalid' );
 
-		$this->assertNotEmpty( $data['url'], 'Theme URL does not exist' );
-		$this->assertRegExp( '/^(https?):\/\/[^\s\/$.?#].[^\s]*$/i', $data['url'], 'Theme URL format is invalid' );
-		$this->assertContains( $data['theme'], $data['url'], 'Theme URL does not contain the theme slug' );
+		$this->assertNotEmpty( $data['url'], 'Theme demo URL does not exist' );
+		$this->assertRegExp( '/^(https):\/\/[^\s\/$.?#].[^\s]*$/i', $data['url'], 'Theme demo URL format is invalid' );
 
 		$this->assertNotEmpty( $data['package'], 'Theme package URL does not exist' );
-		$this->assertRegExp( '/^(https?):\/\/[^\s\/$.?#].[^\s]*$/i', $data['package'], 'Theme package URL format is invalid' );
-		$this->assertContains( "/godaddy/wp-{$data['theme']}-theme/", $data['package'], 'Theme package URL does not contain the theme repo' );
-		$this->assertContains( "/v{$data['new_version']}/", $data['package'], 'Theme package URL does not contain the theme version' );
-		$this->assertStringEndsWith( "/{$data['theme']}.zip", $data['package'], 'Theme package URL does not point to a ZIP file of the theme slug' );
+		$this->assertRegExp( '/^(https):\/\/[^\s\/$.?#].[^\s]*$/i', $data['package'], 'Theme package URL format is invalid' );
+		$this->assertStringEndsWith( "{$data['theme']}.zip", $data['package'], 'Theme package URL does not point to a ZIP file of the theme slug' );
 
 		$this->assertNotEmpty( $data['screenshot'], 'Theme screenshot URL does not exist' );
-		$this->assertRegExp( '/^(https?):\/\/[^\s\/$.?#].[^\s]*$/i', $data['screenshot'], 'Theme screenshot URL format is invalid' );
-		$this->assertContains( "/v{$data['new_version']}/", $data['screenshot'], 'Theme screenshot URL does not contain the theme version' );
-		$this->assertStringEndsWith( "/screenshot.png", $data['screenshot'], 'Theme screenshot URL does not point to a `screenshot.png` file' );
+		$this->assertRegExp( '/^(https):\/\/[^\s\/$.?#].[^\s]*$/i', $data['screenshot'], 'Theme screenshot URL format is invalid' );
+		$this->assertStringContainsString( $data['theme'], $data['screenshot'], 'Theme screenshot URL does not contain the theme slug' );
+		$this->assertStringEndsWith( "screenshot.png", $data['screenshot'], 'Theme screenshot URL does not point to a `screenshot.png` file' );
 
 		$this->assertNotEmpty( $data['name'], 'Theme name does not exist' );
 		$this->assertEquals( strtolower( str_replace( ' ', '-', $data['name'] ) ), $data['theme'], 'Theme name and slug are not similar' );
@@ -77,8 +74,17 @@ class Tests extends TestCase
 	 */
 	public function testUrlReachable( $data )
 	{
+		// Skip SSL validation.
+		stream_context_set_default(
+			[
+				'ssl' => [
+					'verify_peer'      => false,
+					'verify_peer_name' => false,
+				],
+			]
+		);
 		$headers = get_headers( $data['url'] );
-		$this->assertContains( '200 OK', $headers[0], 'Theme URL is unreachable' );
+		$this->assertStringContainsString( '200 OK', $headers[0], 'Theme demo URL is unreachable' );
 	}
 
 	/**
@@ -86,8 +92,17 @@ class Tests extends TestCase
 	 */
 	public function testPackgeReachable( $data )
 	{
+		// Skip SSL validation.
+		stream_context_set_default(
+			[
+				'ssl' => [
+					'verify_peer'      => false,
+					'verify_peer_name' => false,
+				],
+			]
+		);
 		$headers = get_headers( $data['package'] );
-		$this->assertContains( '302 Found', $headers[0], 'Theme package URL is unreachable' );
+		$this->assertRegExp( '/(200 OK|302 Found)/', $headers[0], 'Theme package URL is unreachable' );
 	}
 
 	/**
@@ -95,7 +110,16 @@ class Tests extends TestCase
 	 */
 	public function testScreenshotReachable( $data )
 	{
+		// Skip SSL validation.
+		stream_context_set_default(
+			[
+				'ssl' => [
+					'verify_peer'      => false,
+					'verify_peer_name' => false,
+				],
+			]
+		);
 		$headers = get_headers( $data['screenshot'] );
-		$this->assertContains( '200 OK', $headers[0], 'Theme screenshot URL is unreachable' );
+		$this->assertRegExp( '/(200 OK|302 Found)/', $headers[0], 'Theme screenshot URL is unreachable' );
 	}
 }
