9f47eff3e1ec0c93611c85622f1d4627da81f458
Evan Herman <evan.m.herman@gmail.com>
2020-03-24 12:38:54 -0400

    Update manifest and tests

--- tests.php
+++ tests.php
@@ -17,69 +17,12 @@ class Tests extends TestCase
 		);
 	}
 
-	public function testFilesExist()
-	{
-		$this->assertTrue( is_readable( self::$manifest ), 'Manifest file is not readable' );
-		$this->assertTrue( is_readable( self::$manifest_min ), 'Minified manifest file is not readable' );
-	}
-
-	public function testFileContents()
-	{
-		$json     = (string) file_get_contents( self::$manifest );
-		$json_min = (string) file_get_contents( self::$manifest_min );
-
-		$this->assertJson( $json, 'Manifest is not valid JSON' );
-		$this->assertJson( $json_min, 'Minified manifest is not valid JSON' );
-
-		$manifest     = json_decode( $json, true );
-		$manifest_min = json_decode( $json_min, true );
-
-		$this->assertNotEmpty( $manifest, 'Manifest is empty' );
-		$this->assertNotEmpty( $manifest_min, 'Minified manifest is empty' );
-		$this->assertEquals( $manifest, $manifest_min, 'Manifests do not match' );
-	}
-
 	/**
 	 * @dataProvider provideThemeData
 	 */
-	public function testThemeData( $data )
+	public function testThemeExistsOnWpOrg( $theme_slug )
 	{
-		$this->assertTrue( is_array( $data ), 'Theme data is not an array' );
-		$this->assertCount( 6, $data, 'Theme data array does not contain exactly 6 items' );
 
-		$this->assertNotEmpty( $data['theme'], 'Theme slug does not exist' );
-		$this->assertTrue( ctype_lower( str_replace( '-', '', $data['theme'] ) ), 'Theme slug is not lowercase' );
-
-		$this->assertNotEmpty( $data['new_version'], 'Theme version does not exist' );
-		$this->assertRegExp( '/^([\w\.\-]+)$/', $data['new_version'], 'Theme version format is invalid' );
-
-		$this->assertNotEmpty( $data['url'], 'Theme demo URL does not exist' );
-		$this->assertRegExp( '/^(https):\/\/[^\s\/$.?#].[^\s]*$/i', $data['url'], 'Theme demo URL format is invalid' );
-
-		$this->assertNotEmpty( $data['package'], 'Theme package URL does not exist' );
-		$this->assertRegExp( '/^(https):\/\/[^\s\/$.?#].[^\s]*$/i', $data['package'], 'Theme package URL format is invalid' );
-
-		// Try for a versioned file, or latest-stable.zip
-		try {
-			$this->assertRegExp( sprintf( '/wordpress\.org\/theme\/%s\.%s\.zip$/i', preg_quote( $data['theme'] ), preg_quote( $data['new_version'] ) ), $data['package'], 'Theme package URL does not point to a versioned ZIP file of the theme slug' );
-		} catch ( \Exception $exception ) {
-			$this->assertRegExp( sprintf( '/wordpress\.org\/theme\/%s\.latest-stable\.zip$/i', preg_quote( $data['theme'] ) ), $data['package'], 'Theme package URL does not point to a versioned ZIP file of the theme slug' );
-		}
-
-		$this->assertNotEmpty( $data['screenshot'], 'Theme screenshot URL does not exist' );
-		$this->assertRegExp( '/^(https):\/\/[^\s\/$.?#].[^\s]*$/i', $data['screenshot'], 'Theme screenshot URL format is invalid' );
-		$this->assertStringContainsString( $data['theme'], $data['screenshot'], 'Theme screenshot URL does not contain the theme slug' );
-		$this->assertStringEndsWith( "screenshot.png", $data['screenshot'], 'Theme screenshot URL does not point to a `screenshot.png` file' );
-
-		$this->assertNotEmpty( $data['name'], 'Theme name does not exist' );
-		$this->assertEquals( strtolower( str_replace( ' ', '-', $data['name'] ) ), $data['theme'], 'Theme name and slug are not similar' );
-	}
-
-	/**
-	 * @dataProvider provideThemeData
-	 */
-	public function testUrlReachable( $data )
-	{
 		// Skip SSL validation.
 		stream_context_set_default(
 			[
@@ -89,43 +32,10 @@ class Tests extends TestCase
 				],
 			]
 		);
-		$headers = get_headers( $data['url'] );
-		$this->assertStringContainsString( '200 OK', $headers[0], 'Theme demo URL is unreachable' );
-	}
 
-	/**
-	 * @dataProvider provideThemeData
-	 */
-	public function testPackgeReachable( $data )
-	{
-		// Skip SSL validation.
-		stream_context_set_default(
-			[
-				'ssl' => [
-					'verify_peer'      => false,
-					'verify_peer_name' => false,
-				],
-			]
-		);
-		$headers = get_headers( $data['package'] );
+		$headers = get_headers( "https://downloads.wordpress.org/theme/${theme_slug}.latest-stable.zip" );
+
 		$this->assertRegExp( '/(200 OK|302 Found)/', $headers[0], 'Theme package URL is unreachable' );
-	}
 
-	/**
-	 * @dataProvider provideThemeData
-	 */
-	public function testScreenshotReachable( $data )
-	{
-		// Skip SSL validation.
-		stream_context_set_default(
-			[
-				'ssl' => [
-					'verify_peer'      => false,
-					'verify_peer_name' => false,
-				],
-			]
-		);
-		$headers = get_headers( $data['screenshot'] );
-		$this->assertRegExp( '/(200 OK|302 Found)/', $headers[0], 'Theme screenshot URL is unreachable' );
 	}
 }
